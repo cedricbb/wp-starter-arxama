72032094961a39fcbc1c275d9ce487af59a615b7
Cédric BILLARD <cbillard@com-company.fr>
2026-01-23 14:17:54 +0100

    .aidigest + .idea added to .gitignore

--- .aidigest/theme/theme/Model/Widgets/Assets/scripts/WidgetName.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function () {
-    jQuery(window).on('elementor/frontend/init', function () {
-        elementorFrontend.hooks.addAction('frontend/element_ready/WidgetName.default', function ($scope) {
-            //
-        });
-    });
-});
